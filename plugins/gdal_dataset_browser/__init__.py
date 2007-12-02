dc93bbb09378fee85ddd47c87cae8eacbef6ab19
Antonio Valentino <a_valentino@users.sf.net>
2007-12-02 11:28:46 +0000

    gdal_dataset_browser module renamed dataset_browser. Improved plugin headers.
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/gsdview/trunk@42 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

--- plugins/gdal_dataset_browser/__init__.py
+++ plugins/gdal_dataset_browser/__init__.py
@@ -1,6 +1,27 @@
+### Copyright (C) 2006-2007 Antonio Valentino <a_valentino@users.sf.net>
+
+### This file is part of GSDView.
+
+### GSDView is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 2 of the License, or
+### (at your option) any later version.
+
+### GSDView is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+
+### You should have received a copy of the GNU General Public License
+### along with GSDView; if not, write to the Free Software
+### Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+'''Browser component for GDAL datasets.'''
+
+__author__  = 'Antonio Valentino <a_valentino@users.sf.net>'
+__date__    = '$Date$'
 __version__ = (1,0,0)
-__revision__ = '$Id: $'
-__author__ = 'Antonio Valentino <a_valentino@users.sf.net>'
+__revision__ = '$Revision$'
 __requires__ = []
 
 def init(mainWin):
