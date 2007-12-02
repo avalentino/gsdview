e11ad8ca2da960c554f6960660f5a6914a0eed7c
Antonio Valentino <a_valentino@users.sf.net>
2007-12-02 19:40:30 +0000

    svn:keywords and svn:ignore properties fixed in zoom_tool.
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/gsdview/trunk@49 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

--- plugins/zoom_tool/__init__.py
+++ plugins/zoom_tool/__init__.py
@@ -19,9 +19,9 @@
 '''Zoom tool.'''
 
 __author__  = 'Antonio Valentino <a_valentino@users.sf.net>'
-__date__    = '$Date: 2007-12-02 17:26:44 +0100 (dom, 02 dic 2007) $'
+__date__    = '$Date$'
 __version__ = (1,0,0)
-__revision__ = '$Revision: 43 $'
+__revision__ = '$Revision$'
 __requires__ = []
 
 from PyQt4 import QtCore
