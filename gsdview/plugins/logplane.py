067cab738359016970b2129d23a747e20a100b31
Antonio Valentino <a_valentino@users.sf.net>
2009-08-16 08:15:59 +0000

    Removed duplicate __revision__.
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/trunk@457 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

--- gsdview/plugins/logplane.py
+++ gsdview/plugins/logplane.py
@@ -22,7 +22,6 @@ __author__   = 'Antonio Valentino <a_valentino@users.sf.net>'
 __date__     = '$Date$'
 __revision__ = '$Revision$'
 __version__  = (0,5,9)
-__revision__ = '$Revision$'
 __requires__ = []
 
 __all__ = ['init', 'close', 'loadSettings', 'saveSettings',
