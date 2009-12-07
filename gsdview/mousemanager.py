7bc1edecae9b3dff924512bf819944d018b8dc8d
Antonio Valentino <a_valentino@users.sf.net>
2009-12-07 17:08:38 +0000

    New mouse manager component.
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/trunk@562 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

---
+++ gsdview/mousemanager.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtCore, QtGui
+
+from gsdview import qt4support
+
+class MouseMode(QtCore.QObject):
+    dragmode = QtGui.QGraphicsView.NoDrag
+    cursor = None
+    icon = QtGui.QIcon()
+    label = ''
+    name = ''
+
+    def eventFilter(self, object, event):
+        if isinstance(object, QtGui.QGraphicsScene):
+            return self.sceneEventFilter(object, event)
+        elif isinstance(object, QtGui.QGraphicsView):
+            if event.type() == QtCore.QEvent.Enter:
+                object.setDragMode(self.dragmode)
+                if self.cursor:
+                    object.setCursor(self.cursor)
+                else:
+                    object.unsetCursor()
+            return self.viewEventFilter(object, event)
+        elif isinstance(object, QtGui.QScrollBar):
+            return self.scrollbarEventFilter(object, event)
+        else:
+            return False
+
+    def sceneEventFilter(self, object, event):
+        return False
+
+    def viewEventFilter(self, object, event):
+        if event.type() == QtCore.QEvent.Enter:
+            object.setDragMode(self.dragmode)
+            if self.cursor:
+                object.setCursor(self.cursor)
+            else:
+                object.unsetCursor()
+
+        return False
+
+    def scrollbarEventFilter(self, object, event):
+        return False
+
+class PointerMode(MouseMode):
+    dragmode = QtGui.QGraphicsView.NoDrag
+    cursor = None
+    icon = qt4support.geticon('arrow.svg', __name__)
+    label = 'Pointer'
+    name = 'pointer'
+
+class ScrollHandMouseMode(MouseMode):
+    dragmode = QtGui.QGraphicsView.ScrollHandDrag
+    cursor = None
+    icon = qt4support.geticon('hand.svg', __name__)
+    label = 'Scroll hand'
+    name = 'hand'
+
+    def viewEventFilter(self, object, event):
+        if event.type() == QtCore.QEvent.Wheel:
+
+            # Delta is expressed in 1/8 degree
+            delta = event.delta() / 8.  # degree
+
+            # Conversion from degrees to zoom factor:
+            # a factor of 1.1 every 15 degrees
+            k = 1.1/15.
+
+            if delta >= 0:
+                factor = k * delta
+            else:
+                factor = -1/(k * delta)
+
+            object.scale(factor, factor)
+            event.accept()
+
+            return True
+
+        return False
+
+    def scrollbarEventFilter(self, object, event):
+        # ignore wheel events
+        if event.type() == QtCore.QEvent.Wheel:
+            return True
+        else:
+            return False
+
+class RubberBandMouseMode(MouseMode):
+    dragmode = QtGui.QGraphicsView.RubberBandDrag
+    cursor = QtCore.Qt.CrossCursor
+    icon = qt4support.geticon('area.svg', __name__)
+    label = 'Rubber band'
+    name = 'rubberband'
+
+    def sceneEventFilter(self, object, event):
+        if event.type() == QtCore.QEvent.GraphicsSceneMouseRelease:
+            p0 = event.buttonDownScenePos(QtCore.Qt.LeftButton)
+            p1 = event.scenePos()
+            rect = QtCore.QRectF(p0, p1).normalized()
+            self.emit(QtCore.SIGNAL('rubberBandSeclection(const QRectF&)'),
+                      rect)
+            return True
+
+        #return object.eventFilter(object, event)   # @TODO: check
+        #return QtGui.QGraphicsScene.eventFilter(self, object, event)
+        return False
+
+    def scrollbarEventFilter(self, object, event):
+        # ignore wheel events if some button is pressed
+        if ((event.type() == QtCore.QEvent.Wheel)
+                                and (event.buttons() != QtCore.Qt.NoButton)):
+            return True
+        else:
+            return False
+
+class MouseManager(QtCore.QObject):
+
+    def __init__(self, parent=None):
+        QtCore.QObject.__init__(self, parent)
+
+        self._moderegistry = [
+            PointerMode(),
+            ScrollHandMouseMode(),
+            RubberBandMouseMode(),
+        ]
+
+        self.actions = self._setupActions()
+
+        self.toolbar = QtGui.QToolBar('Mouse')
+        self.toolbar.addActions(self.actions.actions())
+
+        self.menu = QtGui.QMenu('Mouse')
+        self.menu.addActions(self.actions.actions())
+
+    def _newModeAction(self, mode, parent=None):
+        action = QtGui.QAction(mode.icon, self.tr(mode.label), parent)
+        action.setCheckable(True)
+        self.connect(action, QtCore.SIGNAL('triggered()'),
+                     self._emitModeChanged)
+
+        return action
+
+    def _setupActions(self):
+        actions = QtGui.QActionGroup(self)
+        actions.setExclusive(True)
+
+        for index, mode in enumerate(self._moderegistry):
+            action = self._newModeAction(mode, actions)
+            action.setData(QtGui.QGraphicsView.NoDrag)
+
+        actions.actions()[0].setChecked(True)
+
+        return actions
+
+    def _getMode(self):
+        action = self.actions.checkedAction()
+        index = self.actions.actions().index(action)
+        return self._moderegistry[index].name
+
+    def _setMode(self, name):
+        names = self.modes()
+        index = names.index(name)
+        action = self.actions.actions()[index]
+        self.actions.setChecked(True)
+
+    def _delMode(self, name):
+        names = [m.name for m in self._moderegistry]
+        index = names.index(name)
+        action = self.actions.actions()[index]
+        self.actions.removeAction(action)
+        del self._moderegistry[index]
+        #~ if actin.checked() and self._moderegistry:
+            #~ self.actions.actions()[0].setChecked(True)
+
+    mode = property(_getMode, _setMode, _delMode, 'mouse mode name')
+
+    def modes(self):
+        return tuple(m.name for m in self._moderegistry)
+
+    def register(self, mode):
+        action = self._newModeAction(mode, self.actions)
+        self.actions.addAction(action)
+        self._moderegistry.append(mode)
+
+    def _modeIndex(self, name):
+        names = self.modes()
+        return names.index(name)
+
+    def getMode(self, name=None):
+        '''Return the mouse mode object'''
+
+        if name is None:
+            action = self.actions.checkedAction()
+            index = self.actions.actions().index(action)
+        else:
+            index = self._modeIndex(name)
+        return self._moderegistry[index]
+
+    def _emitModeChanged(self, name=None):
+        self.emit(QtCore.SIGNAL('modeChanged(PyQt_PyObject)'), self.mode)
+
+    def eventFilter(self, object, event):
+        '''Events dispatcher'''
+
+        return self.getMode().eventFilter(object, event)
---
+++ gsdview/tests/test_mousemanager.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.pardir, os.pardir))
+
+from PyQt4 import QtCore, QtGui
+
+from gsdview.mousemanager import MouseManager
+
+
+class MainWin(QtGui.QMainWindow):
+
+    def __init__(self, parent=None, flags=QtCore.Qt.Widget):
+        QtGui.QMainWindow.__init__(self, parent, flags)
+
+        self.mousemanager = MouseManager(self)
+
+        self.scene = QtGui.QGraphicsScene(self)
+        self.scene.installEventFilter(self.mousemanager)
+        self.graphicsview = QtGui.QGraphicsView(self.scene, self)
+        self.graphicsview.installEventFilter(self.mousemanager)
+        self.graphicsview.verticalScrollBar().installEventFilter(self.mousemanager)
+        self.setCentralWidget(self.graphicsview)
+
+        # File Actions
+        self.fileactions = self._setupFileActions()
+
+        menu = QtGui.QMenu('File', self)
+        menu.addActions(self.fileactions.actions())
+        self.menuBar().addMenu(menu)
+
+        toolbar = QtGui.QToolBar('File', self)
+        toolbar.addActions(self.fileactions.actions())
+        self.addToolBar(toolbar)
+
+        # Mouse Actions
+        self.menuBar().addMenu(self.mousemanager.menu)
+        self.addToolBar(self.mousemanager.toolbar)
+
+        # Help action
+        self.helpactions = self._setupHelsActions()
+
+        menu = QtGui.QMenu('Help', self)
+        menu.addActions(self.helpactions.actions())
+        self.menuBar().addMenu(menu)
+
+        toolbar = QtGui.QToolBar('Help', self)
+        toolbar.addActions(self.helpactions.actions())
+        self.addToolBar(toolbar)
+
+        self.resize(700, 500)
+
+    def _setupFileActions(self):
+        style = self.style()
+
+        actions = QtGui.QActionGroup(self)
+
+        icon = style.standardIcon(QtGui.QStyle.SP_DialogOpenButton)
+        action = QtGui.QAction(icon, 'Open', actions)
+        self.connect(action, QtCore.SIGNAL('triggered()'), self.openfile)
+
+        icon = style.standardIcon(QtGui.QStyle.SP_DialogCloseButton)
+        action = QtGui.QAction(icon, 'Close', actions)
+        self.connect(action, QtCore.SIGNAL('triggered()'), self.scene.clear)
+
+        QtGui.QAction(actions).setSeparator(True)
+
+        icon = style.standardIcon(QtGui.QStyle.SP_DialogCancelButton)
+        action = QtGui.QAction(icon, 'Exit', actions)
+        self.connect(action, QtCore.SIGNAL('triggered()'), self.close)
+
+        return actions
+
+    def _setupHelsActions(self):
+        style = self.style()
+
+        actions = QtGui.QActionGroup(self)
+
+        icon = QtGui.QIcon(':/trolltech/styles/commonstyle/images/fileinfo-32.png')
+        action = QtGui.QAction(icon, 'About', actions)
+        self.connect(action, QtCore.SIGNAL('triggered()'), self.about)
+
+        icon = QtGui.QIcon(':/trolltech/qmessagebox/images/qtlogo-64.png')
+        action = QtGui.QAction(icon, 'About Qt', actions)
+        self.connect(action, QtCore.SIGNAL('triggered()'),
+                     QtGui.QApplication.aboutQt)
+
+        return actions
+
+    def openfile(self):
+        self.scene.clear()
+        self.graphicsview.setMatrix(QtGui.QMatrix())
+        filename = QtGui.QFileDialog.getOpenFileName()
+        if filename:
+            pixmap = QtGui.QPixmap(filename)
+            item = self.scene.addPixmap(pixmap)
+            self.scene.setSceneRect(item.boundingRect())
+
+    def about(self):
+        title = self.tr('MouseManager Example')
+        text = ['<h1>Mouse Manager</h1>'
+                '<p>Example program for the Mouse manager component.</p>',
+                '<p>Copyright (C): 2009 '
+                '<a href="mailto:a_valentino@users.sf.net">'
+                    'Antonio Valentino'
+                '<a>.</p>']
+        text = self.tr('\n'.join(text))
+        QtGui.QMessageBox.about(self, title, text)
+
+
+def test_mousemanager():
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName('MouseManager')
+    mainwin = MainWin()
+    mainwin.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    test_mousemanager()
