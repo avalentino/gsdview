b71e8884d24cc457ef984cb6f332f80a30ccb3c8
Antonio Valentino <a_valentino@users.sf.net>
2011-07-30 10:37:05 +0000

    Minor changes
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/trunk@972 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

--- exectools/qt4.py
+++ exectools/qt4.py
@@ -18,7 +18,7 @@
 ### along with this module; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-'''Tools for running external processes in a QT4 GUI.'''
+'''Tools for running external processes in a Qt4 GUI.'''
 
 
 import time
