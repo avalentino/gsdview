4e376fb1804532bcc238f86846627eda7732d717
Antonio Valentino <a_valentino@users.sf.net>
2011-11-13 10:44:05 +0000

    Typo
    git-svn-id: https://svn.code.sf.net/p/gsdview/code/trunk@1008 31ebf9c9-8c17-4d41-85ab-37ddf3aa85fe

--- gsdview/plugins/stretch/widgets.py
+++ gsdview/plugins/stretch/widgets.py
@@ -57,7 +57,7 @@ class StretchWidget(QtGui.QWidget, StretchWidgetBase):
         self._floatmode = False
         self.lowSlider.valueChanged.connect(self._onLowSliderChanged)
         self.highSlider.valueChanged.connect(self._onHighSliderChanged)
-        # @COMPATIBILITY: the followin code breaks PySide 1.0.1.
+        # @COMPATIBILITY: the following code breaks PySide 1.0.1.
         #                 It is OK in PyQt4 and PySide 1.0.4
         try:
             self.minSpinBox.valueChanged[float].connect(self._onMinimumChanged)
