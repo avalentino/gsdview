63672dad204dc792934a4e2aeb55b466ed08f659
Antonio Valentino <antonio.valentino@tiscali.it>
2014-12-21 20:38:39 +0100

    Formatting

--- tests/test_graphicsdraw.py
+++ tests/test_graphicsdraw.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-### Copyright (C) 2008-2014 Antonio Valentino <antonio.valentino@tiscali.it>
-
-### This file is part of exectools.
-
-### This module is free software you can redistribute it and/or modify
-### it under the terms of the GNU General Public License as published by
-### the Free Software Foundation either version 2 of the License, or
-### (at your option) any later version.
-
-### This module is distributed in the hope that it will be useful,
-### but WITHOUT ANY WARRANTY without even the implied warranty of
-### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-### GNU General Public License for more details.
-
-### You should have received a copy of the GNU General Public License
-### along with this module if not, write to the Free Software
-### Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
+# GSDView - Geo-Spatial Data Viewer
+# Copyright (C) 2008-2014 Antonio Valentino <antonio.valentino@tiscali.it>
+#
+# This module is free software you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation either version 2 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this module if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
 
 
 import os
