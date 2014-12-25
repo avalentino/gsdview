f8cdf96b3a905ccb21fdebe90512f4d606d895b3
Antonio Valentino <antonio.valentino@tiscali.it>
2014-12-25 21:28:47 +0100

    Move test files from gdalbackend to the tests folder

--- tests/test_ogrqt.py
+++ tests/test_ogrqt.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-### Copyright (C) 2008-2014 Antonio Valentino <antonio.valentino@tiscali.it>
+# GSDView - Geo-Spatial Data Viewer
+# Copyright (C) 2008-2014 Antonio Valentino <antonio.valentino@tiscali.it>
+#
+# This module is free software you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation either version 2 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this module if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
 
-### This file is part of exectools.
-
-### This module is free software; you can redistribute it and/or modify
-### it under the terms of the GNU General Public License as published by
-### the Free Software Foundation; either version 2 of the License, or
-### (at your option) any later version.
-
-### This module is distributed in the hope that it will be useful,
-### but WITHOUT ANY WARRANTY; without even the implied warranty of
-### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-### GNU General Public License for more details.
-
-### You should have received a copy of the GNU General Public License
-### along with this module; if not, write to the Free Software
-### Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
 
 import os
 import sys
@@ -25,11 +25,12 @@ import logging
 
 from osgeo import ogr
 
+
 # Fix sys path
-from os.path import abspath, dirname
-GSDVIEWROOT = abspath(os.path.join(dirname(__file__),
-                                   os.pardir, os.pardir, os.pardir))
+GSDVIEWROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.insert(0, GSDVIEWROOT)
+
 
 from qt import QtCore, QtWidgets, QtGui
 
