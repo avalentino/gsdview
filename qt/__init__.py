5c0052d4ddd166c766f1714919f20fe0e7cc5195
Antonio Valentino <antonio.valentino@tiscali.it>
2015-01-02 10:09:51 +0100

    Update license statements in qt package

--- qt/__init__.py
+++ qt/__init__.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# -----------------------------------------------------------------------------
-# Copyright (c) 2010, Enthought Inc
+# Copyright (c) 2006-2010 Enthought, Inc.
+# Copyright (c) 2011-2015 Antonio Valentino <antonio.valentino@tiscali.it>
 # All rights reserved.
 #
-# This software is provided without warranty under the terms of the BSD
-# license.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of copyright holder nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
 #
-# Author: Enthought Inc
-# Description: Qt API selector. Can be used to switch between pyQt and PySide
-# -----------------------------------------------------------------------------
-
-# -----------------------------------------------------------------------------
-# NOTE: Code modified to make PyQt4 the default Qt wrapper.
-# -----------------------------------------------------------------------------
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
 
 
 def prepare_pyqt4():
-    # Set PySide compatible APIs.
+    # Set API version 2 (compatible with both PyQt5 and PySide)
     import sip
 
     sip.setapi('QDate', 2)
