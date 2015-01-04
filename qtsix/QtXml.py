577c96011d3562a8c6107c30b048a58d26e1d38f
Antonio Valentino <antonio.valentino@tiscali.it>
2015-01-04 21:48:27 +0100

    The internal copy of qtsix has been update to v1.1

--- qtsix/QtXml.py
+++ qtsix/QtXml.py
@@ -32,30 +32,11 @@
 
 from . import qt_api
 
-__all__ = [
-    'QWebDatabase',
-    'QWebElement',
-    'QWebElementCollection',
-    'QWebHistory',
-    'QWebHistoryInterface',
-    'QWebHistoryItem',
-    'QWebPluginFactory',
-    'QWebSecurityOrigin',
-    'QWebSettings',
-    'qWebKitMajorVersion',
-    'qWebKitMinorVersion',
-    'qWebKitVersion',
-]
-
 if qt_api == 'pyqt5':
-    from PyQt5.QtWebKit import *
+    from PyQt5.QtXml import *
 
 elif qt_api == 'pyqt4':
-    import PyQt4.QtWebKit as _QtWebKit
-    locals().update(dict((name, getattr(_QtWebKit, name)) for name in __all__))
-    del _QtWebKit
+    from PyQt4.QtXml import *
 
 elif qt_api == 'pyside':
-    import PySide.QtWebKit as _QtWebKit
-    locals().update(dict((name, getattr(_QtWebKit, name)) for name in __all__))
-    del _QtWebKit
+    from PySide.QtXml import *
